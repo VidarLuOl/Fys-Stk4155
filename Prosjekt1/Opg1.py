1c3d112a4794bb6c39db49591216ae2f753954fa
VidarLuOl <vidarlol@student.matnat.uio.no>
2021-09-13 11:51:22 +0200

    Prøve å fjerna fil

--- Prosjekt1/Opg1.py
+++ Prosjekt1/Opg1.py
@@ -1,5 +1,6 @@
 from Funksjoner import *
 
+#For FrankeFunction
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -7,28 +8,30 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from random import random, seed
 
+#Andre imports
+from scipy.stats import norm
+
 fig = plt.figure()
 ax = fig.gca(projection="3d")
-
 
 # Make data.
 x = np.arange(0, 1, 0.05)
 y = np.arange(0, 1, 0.05)
 x, y = np.meshgrid(x,y)
 
+# print(np.random.randn(5,5))
+print(norm())
 
-z = FrankeFunction(x, y)
+# z = FrankeFunction(x, y)
 
+# # Plot the surface.
+# surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+# # Customize the z axis.
+# ax.set_zlim(-0.10, 1.40)
+# ax.zaxis.set_major_locator(LinearLocator(10))
+# ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
 
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
-
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
+# # Add a color bar which maps values to colors.
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+# plt.show()
