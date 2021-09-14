244ef7b948099cb6e3607cc49ca89f0aaa05db15
VidarLuOl <vidarlol@student.matnat.uio.no>
2021-09-14 10:34:56 +0200

    Gjort opg1 fram til skaleringen

--- Prosjekt1/Funksjoner.py
+++ Prosjekt1/Funksjoner.py
@@ -7,6 +7,40 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
+def OLS(x, y, z, p, n):
+    XD = Design_X(x, y, p) #Designmatrisen blir laget her
+
+    z = np.ravel(z)  #Vi er nødt til å gjøre om y til en vektor ettersom det er en matrise før.
+
+    beta = betaFunc(XD, z) #Vi finner beta verdiene her
+
+    ztilde = XD @ beta
+    ztilde_plot = np.reshape(ztilde, (n,n))
+
+    MeanSE = MSE(z, ztilde)
+
+    R2_score = R2(z, ztilde)
+    return MeanSE, R2_score, ztilde_plot
+
+"""___________________________________OSL FUNKSJONER________________________________"""
+def Design_X(x, y, p):
+    if len(x.shape)>1:
+        x = np.ravel(x)
+        y = np.ravel(y)
+    
+    r = len(x)                  #Antall rader
+    c = int((p+1)*(p+2)/2)      #Antall kolonner
+    X = np.ones((r, c))         #Lager en matrise hvor alle verdiene er 1
+
+    for i in range(1, p+1):             #looping over the degrees but we have 21 elements
+        q = int(i*(i+1)/2)              #Gjør det slik at vi 
+        for k in range(i+1):            #this loop makes sure we dont fill the same values in X over and over
+            X[:,q+k] = x**(i-k)*y**k    #using the binomial theorem to get the right exponents
+    return X
+
+def betaFunc(X, y):
+    return np.linalg.inv(X.T @ X) @ (X.T @ y)
+
 def MSE(y, ytilde):
     #MSE er når vi skal sjekke hvor nøyaktig modellen vi har lagd er iforhold til punktene vi har fått inn
     #Jo nærmere 0 jo bedre er MSE, hvis den er 0 så er den mest sannsynlig overfittet pga at normalt vil støy ødellege.
@@ -25,3 +59,30 @@ def R2(y, ytilde):
         u += (y[i] - ytilde[i])**2
         l += (y[i] - m)**2
     return 1 - u/l
+"""______________________________________________________________________________________________"""
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+def Scaled_OSL(x, y, z, p, n):
+    scaler = StandardScaler()
+    
+    XD = Design_X(x, y, p) #Designmatrisen blir laget her
+
+    scaler.fit(XD)
+    XD = scaler.transform(XD)
+    XD[:, 0] = 1
+
+    
+
+    # z = np.ravel(z)  #Vi er nødt til å gjøre om y til en vektor ettersom det er en matrise før.
+
+    # beta = betaFunc(XD, z) #Vi finner beta verdiene her
+
+    # ztilde = XD @ beta
+    # ztilde_plot = np.reshape(ztilde, (n,n))
+
+    # MeanSE = MSE(z, ztilde)
+
+    # R2_score = R2(z, ztilde)
+    return "hei"
--- Prosjekt1/Opg1.py
+++ Prosjekt1/Opg1.py
@@ -9,29 +9,49 @@ import numpy as np
 from random import random, seed
 
 #Andre imports
-from scipy.stats import norm
+
+
+"""_________________________Variabler_______________________"""
+n = 5          #Antall punkter i x- og y-retning på modellen
+noise = 0.1     #Hvor mye støy påvirker modellen 
+p = 2           #Grad av polynom
+
+"""_________________________________________________________"""
+
 
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 
 # Make data.
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
+x = np.arange(0, 1, 1/n)
+y = np.arange(0, 1, 1/n)
 x, y = np.meshgrid(x,y)
 
-# print(np.random.randn(5,5))
-print(norm())
+noise_full = noise*np.random.randn(n, n)
 
-# z = FrankeFunction(x, y)
 
-# # Plot the surface.
-# surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+if __name__ == "__main__":
+    z = FrankeFunction(x, y) + noise_full
 
-# # Customize the z axis.
-# ax.set_zlim(-0.10, 1.40)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+    MeanSE, R2_score, ztilde_plot = OLS(x, y, z, p, n)
 
-# # Add a color bar which maps values to colors.
-# fig.colorbar(surf, shrink=0.5, aspect=5)
-# plt.show()
+    # print("Grad = %i" %p)
+    # print("MSE = %.6f" %MeanSE)
+    # print("R2 = %.6f" %R2_score)
+
+    # # Plot the surface.
+    # surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    # surf = ax.plot_surface(x, y, ztilde_plot, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+
+    # # Customize the z axis.
+    # ax.set_zlim(-0.10, 1.40)
+    # ax.zaxis.set_major_locator(LinearLocator(10))
+    # ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+
+    # # Add a color bar which maps values to colors.
+    # fig.colorbar(surf, shrink=0.5, aspect=5)
+    # plt.show()
+
+    """Fra herfra så prøver vi å skalere punktene"""
+    Scaled = Scaled_OSL(x, y, z, p, n)
+
