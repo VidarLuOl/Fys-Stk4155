e2cd628c58da7afce17327f505b4dd61f4227189
thobergh <thobergh@student.matnat.uio.no>
2021-09-22 13:43:06 +0200

    Create Functions.py
    Yes

---
+++ Prosjekt1/Functions.py
@@ -0,0 +1,45 @@
+#Functions
+import numpy as np
+import matplotlib.pyplot as plt
+
+def FrankeFunction(x,y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
+
+
+def create_X(x, y, n):
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+
+	N = len(x)
+	l = int((n+1)*(n+2)/2)
+	X = np.ones((N,l))
+
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = (x**(i-k))*(y**k)
+
+	return X
+
+
+
+def beta(X,y):
+    return np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+
+
+
+def plot(x,y, label, ylab):
+    plt.figure()
+    plt.title(label)
+    plt.xlabel("polynom order")
+    plt.ylabel(ylab)
+    plt.plot(x,y)
+    
+    
+    
