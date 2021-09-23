bf070cef02e67727b87ba79c6c433eedc8b66fb4
thobergh <thobergh@student.matnat.uio.no>
2021-09-23 15:57:17 +0200

    Update Exercise2.py
    Booom

--- Prosjekt1/Exercise2.py
+++ Prosjekt1/Exercise2.py
@@ -1,15 +1,6 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures, StandardScaler
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from Functions import FrankeFunction, create_X, beta, plot
-from Exercise1 import OLS
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from Functions import FrankeFunction, OLS, plot
+import matplotlib.pyplot as plt
 
 np.random.seed(2021)
 
@@ -21,25 +12,16 @@ x, y = np.meshgrid(x,y)
 z = FrankeFunction(x,y)
 z_noise = FrankeFunction(x,y) + 0.05*np.random.randn(20,20)
 
-#P = 15
-#polynomials = [_ for _ in range(1,P+1)]
-polynomials = [15]
+P = 5
+polynomials = [_ for _ in range(1,P+1)]
 data = OLS(x,y,z_noise,polynomials)
 
 
 
-
-
     
-plot(polynomials,data[0], "MSE train", "mse score")
+testlist = [data[0], data[2]]
 
-#plot(polynomials,data[1], "R2 train", "r2 score")
-plot(polynomials,data[2], "MSE test", "mse score")
-#plot(polynomials,data[3], "R2 test", "r2 score")
-
-"""
-plot(polynomials,data[5], "MSE train scaled", "mse score")
-plot(polynomials,data[6], "R2 train scaled", "r2 score")
-plot(polynomials,data[7], "MSE test scaled", "mse score")
-plot(polynomials,data[8], "R2 test scaled", "r2 score")        
-"""
+plt.figure()
+for y in testlist:
+    plt.plot(polynomials, y)
+plt.show()
